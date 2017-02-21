d44c9ff6501ad747e72b51e5966f8bc88eb7c201
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2017-02-20 23:27:46 -0300

    Entendendo state

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,16 @@
 import CcUsers from './components/users/main.vue'
 
 export default {
+  mounted() {
+    const payload = {
+      name: 'Rafael Ribeiro de Sousa',
+      email: 'faelribeirodesousa@gmail.com',
+      level: 'Dono do mundo'
+  }
+  setTimeout(() => {
+    this.$store.commit('CHANGE_USER', payload)
+  },1000)
+  },
   components: {
     CcUsers
   },
